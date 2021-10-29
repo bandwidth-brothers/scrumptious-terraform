34032b909c02923cbd5ef9ee5613b73091bff14a
joshuagreenss <87674718+joshuagreenss@users.noreply.github.com>
2021-10-29 06:29:11 -0700

    Initial subnet main commit

--- modules/subnet/main.tf
+++ modules/subnet/main.tf
@@ -1,1 +1,9 @@
+resource "aws_subnet" "subnet"{
+  count = 2
+  vpc_id = var.vpc_id
+  cidr_block = "10.0.${count.index}.0/24"
+  availability_zone = "${data.aws_availability_zones.zones.names[count.index]}"
+  map_public_ip_on_launch = var.public
+}
 
+data "aws_availability_zones" "zones"{}
